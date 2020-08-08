cf4d78c21c9802addab25e7fda573c94a9a8994b
Nic Pottier <nicpottier@gmail.com>
2020-08-07 22:29:49 -0700

    add option to connect to de1

--- cmd/des/main.go
+++ cmd/des/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"encoding/hex"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/nicpottier/decent/parser"
 	_ "github.com/nicpottier/decent/types"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var server = flag.String("server", ":8080", "what address and port to start the HTTP server on")
+var de1 = flag.String("de1", "", "what ip and port the de1 is on")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
@@ -19,6 +28,8 @@ func serveDebug(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	flag.Parse()
+
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/debug", serveDebug)
@@ -26,7 +37,36 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	err := http.ListenAndServe(*addr, nil)
+
+	if *de1 != "" {
+		fmt.Printf("connecting to %s\n", *de1)
+		conn, err := net.Dial("tcp", *de1)
+		if err != nil {
+			panic(fmt.Sprintf("unable to connect to de1: %s", err.Error()))
+		}
+		fmt.Printf("connected!\n")
+
+		reader := bufio.NewReader(conn)
+		go func() {
+			for {
+				mt, mb, err := parser.ReadNextToken(reader)
+				fmt.Printf("[%s]%s\n", mt, strings.ToUpper(hex.EncodeToString(mb)))
+				if err == io.EOF {
+					fmt.Println("de1 connectioni closed")
+					return
+				}
+				if err != nil {
+					fmt.Printf("error: %s", err.Error())
+					time.Sleep(100 * time.Millisecond)
+					continue
+				}
+
+				hub.broadcast <- []byte(fmt.Sprintf("[%s]%s", mt, strings.ToUpper(hex.EncodeToString(mb))))
+			}
+		}()
+	}
+
+	err := http.ListenAndServe(*server, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
