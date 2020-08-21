ca22ed0e8a660390470b80dc6372c1e230da7ddc
Nic Pottier <nicpottier@gmail.com>
2020-08-21 11:07:47 -0700

    add reconnection logic to ws and de1 connections, generic file serving

--- cmd/des/main.go
+++ cmd/des/main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"regexp"
+	"strings"
 	"time"
 
 	"go.bug.st/serial"
@@ -21,59 +22,88 @@ import (
 var server = flag.String("server", "0.0.0.0:8080", "what address and port to start the HTTP server on")
 var de1 = flag.String("de1", "", "filesystem path (e.g. for a serial device)  or server ip/hostname:port for connecting to the de1")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./assets/html/home.html")
+func serveRoots(w http.ResponseWriter, r *http.Request) {
+	// we only deal with roots
+	if strings.Count(r.URL.Path, "/") > 1 {
+		http.Error(w, "not found", http.StatusNotFound)
+	}
+
+	path := r.URL.Path
+	if path == "/" {
+		path = "index"
+	}
+	path = strings.TrimPrefix(path, "/")
+	http.ServeFile(w, r, fmt.Sprintf("./static/%s.html", path))
 }
 
-func serveDebug(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./assets/html/debug.html")
+var tcpRegexp = regexp.MustCompile(`[a-zA-Z0-9.]+:[0-9]+`)
+
+// connects to the DE1 at the passed in location. loc can be either a domain/ip:port or
+// a path to a file to serial port
+func connectToDE1(loc string) (io.ReadCloser, error) {
+	if tcpRegexp.MatchString(loc) {
+		return net.Dial("tcp", loc)
+	}
+
+	m := &serial.Mode{
+		BaudRate: 115200,
+	}
+	return serial.Open(loc, m)
 }
 
 func main() {
 	flag.Parse()
 
+	// start our hub
 	h := hub.New()
 	go h.Run()
-	http.HandleFunc("/debug", serveDebug)
-	http.HandleFunc("/", serveHome)
+
+	// then our webserver
+
+	// convenience rewritter for "apps" with root html files.. /index will be rewritten to ./static/index.html
+	http.HandleFunc("/", serveRoots)
+
+	// server ./static as /static
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// expose web socket at /ws
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		hub.ServeWS(h, w, r)
 	})
 
+	// we have a de1 to connect to, do so
 	if *de1 != "" {
-		fmt.Printf("connecting to %s\n", *de1)
-		tcpRegexp := regexp.MustCompile(`[a-zA-Z0-9.]+:[0-9]+`)
-		var conn io.Reader
-		var err error
-		if tcpRegexp.MatchString(*de1) {
-			conn, err = net.Dial("tcp", *de1)
-		} else {
-			m := &serial.Mode{
-				BaudRate: 115200,
-			}
-			conn, err = serial.Open(*de1, m)
-		}
-		if err != nil {
-			panic(fmt.Sprintf("unable to connect to de1: %s", err.Error()))
-		}
-		fmt.Printf("connected!\n")
-
-		reader := bufio.NewReader(conn)
 		go func() {
 			for {
-				ms, err := parser.ReadNextToken(reader)
-				fmt.Printf("%s\n", ms)
-				if err == io.EOF {
-					fmt.Println("de1 connectioni closed")
-					return
-				}
+				fmt.Printf("connecting to: %s\n", *de1)
+				conn, err := connectToDE1(*de1)
+				defer conn.Close()
+
 				if err != nil {
-					fmt.Printf("error: %s", err.Error())
-					time.Sleep(100 * time.Millisecond)
-					continue
+					fmt.Printf("error connecting: %s\n", err.Error())
+				} else {
+					fmt.Println("connected!")
+					reader := bufio.NewReader(conn)
+					for {
+						ms, err := parser.ReadNextToken(reader)
+						fmt.Printf("%s\n", ms)
+						if err == io.EOF {
+							fmt.Println("de1 connectioni closed")
+							break
+						}
+						if err != nil {
+							fmt.Printf("error: %s", err.Error())
+							time.Sleep(100 * time.Millisecond)
+							continue
+						}
+
+						h.Broadcast <- ms
+					}
 				}
 
-				h.Broadcast <- ms
+				fmt.Println("sleeping before reconnecting..")
+				time.Sleep(5 * time.Second)
 			}
 		}()
 	}
@@ -81,6 +111,6 @@ func main() {
 	fmt.Printf("listening on %s\n", *server)
 	err := http.ListenAndServe(*server, nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("Error while serving: ", err)
 	}
 }
